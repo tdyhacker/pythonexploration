ce30ec8ea2957234e376127044a801b2a3116209
kusinwolf <kusinwolf@c446d5ca-c62c-11dd-92e3-e3085bfe42ca>
2008-12-23 23:34:30 +0000

    - Updated xref table - removed tags column from Saved_Links table - Converted link_id to ForeignKey - Inserted Tags manually into Tag table

--- kusinwolf/PylonsApps/helloworld/helloworld/lib/libtool.py
+++ kusinwolf/PylonsApps/helloworld/helloworld/lib/libtool.py
@@ -1,5 +1,19 @@
 from re import compile
 
+
+#from helloworld.model.meta import *
+#from helloworld.model.tags_table import *
+#from helloworld.model.link_table import *
+#
+#tags=['bibliography', 'casual', 'color', 'colorwheel', 'div', 'economics', 'engine', 'experiement', 'facepunch', 'fleetbattles', 'forums', 'from', 'game', 'garrysmod', 'generator', 'gmail', 'gmod', 'google', 'hex', 'mail', 'mmo', 'music', 'old;', 'online', 'pandora', 'radio', 're', 'reference', 'regularexpression', 'resource', 'rpg', 'science', 'search', 'select', 'ships', 'sifi', 'space', 'sqlite', 'stragey', 'tags', 'tool', 'travian', 'tutorial', 'wiki', 'wow']
+#for tag in tags:
+#    meta.Session.save(Tag(tag))
+#meta.Session.commit()
+
+# Evil List comprension that performs 3 querys in a single line while gathering all the tags for the selected link
+#tags = [meta.Session.query(Tag).filter_by(id=xref.tag_id).first() for xref in meta.Session.query(Link_xref_tag).filter_by(link_id=meta.Session.query(Links).first().id).all()]
+
+
 def linkAppend(name):
     if not compile("http://(.*)").match(name):
         name = "http://%s" % name
