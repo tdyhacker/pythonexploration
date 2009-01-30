f5e4e0dc183b556012a4fbc51af839ae4f05ffd6
kusinwolf <kusinwolf@c446d5ca-c62c-11dd-92e3-e3085bfe42ca>
2009-01-30 02:48:58 +0000

    created full script to decode hamming hex

--- trunk/kusinwolf/CompArch OtherGroup codes.py
+++ trunk/kusinwolf/CompArch OtherGroup codes.py
@@ -1,3 +1,5 @@
+from random import Random
+
 it = 1
 if it == 0:
     d = {
@@ -82,7 +84,8 @@ elif it == 1:
     hammingcon = ""
     stream = []
     #text = "lol pwnsauce"
-    text = "rat trap"
+    #text = "rat trap"
+    text = "i is tea pot" 
 
     stream.append("stx")
     for char in text:
@@ -170,12 +173,179 @@ elif it == 1:
 
             hamming += hexvalues[hammingcon[0:4]] + hexvalues[hammingcon[4:8]] + hexvalues[hammingcon[8:]]
 
-            hamming += ":"
+            if len(hamming.split(":")) != len(bTohex.split(":")):
+                hamming += ":"
             
             hammingcon = ""
             thamming = ""
             t = ""
-    print "Hammming: %s" % hamming
+            
+    correctHamming = hamming
+    hamming = ""
+    hammingcon = ""
+    thamming = ""
+    t = ""
+    changed = [0.0,1.0]
+    def mightChange(value):
+        if (Random().random() < (changed[0] /changed[1])) and ((changed[0] /changed[1]) > 0.5):
+            if value == "0":
+                value = "1"
+            else:
+                value = "0"
+            changed[1] += 1
+        else:
+            changed[0] += 1
+
+        return value
+        
+    for byte in bTohex:
+        if byte != ":":
+            t += byte
+        if len(t) == 2:
+            thamming += hexvalues[t[0]] + hexvalues[t[1]]
+            #position =   0  123  4567
+            #size     = 1234,5678,9012
+            #pow      = 12 4    8
+            
+            # Check bet, 1
+            if (int(thamming[0]) + int(thamming[1]) + int(thamming[3]) + int(thamming[4]) + int(thamming[6])) % 2 == 0:
+                hammingcon += "0"
+            else:
+                hammingcon += "1"
+
+            # Check bet, 2
+            if (int(thamming[0]) + int(thamming[2]) + int(thamming[3]) + int(thamming[5]) + int(thamming[6])) % 2 == 0:
+                hammingcon += "0"
+            else:
+                hammingcon += "1"
+                
+            hammingcon += thamming[0]
+
+            # Check bet, 4
+            if (int(thamming[1]) + int(thamming[2]) + int(thamming[3]) + int(thamming[7])) % 2 == 0:
+                hammingcon += "0"
+            else:
+                hammingcon += "1"
+
+            hammingcon += thamming[1] + thamming[2] + thamming[3]
+
+            # Check bet, 8
+            if (int(thamming[4]) + int(thamming[5]) + int(thamming[6]) + int(thamming[7])) % 2 == 0:
+                hammingcon += "0"
+            else:
+                hammingcon += "1"
+
+            hammingcon += thamming[4] + thamming[5] + thamming[6] + thamming[7]
+
+            loc = int(round(Random().random() * (len(hammingcon) - 1)))
+            translate = []
+            for char in hammingcon:
+                translate.append(char)
+            translate[loc] = mightChange(hammingcon[loc])
+            hammingcon = ""
+            for char in translate:
+                hammingcon += char
+
+            hamming += hexvalues[hammingcon[0:4]] + hexvalues[hammingcon[4:8]] + hexvalues[hammingcon[8:]]
+            if len(hamming.split(":")) != len(bTohex.split(":")):
+                hamming += ":"
+            
+            hammingcon = ""
+            thamming = ""
+            t = ""
+
+    print "Safe Hammming:    %s" % correctHamming
+    print "Changed Hammming: %s" % hamming
         
 
-    
+    encodedHamminghex = hamming or ""
+    decodehammingpart = []
+    decodedhamming = ""
+    errors = []
+    triplet = 0
+    for hamhex in encodedHamminghex:
+
+        if hamhex != ":":
+            for hh in hexvalues[hamhex]:
+                decodehammingpart.append( hh )
+            triplet += 1
+        
+            if triplet % 3 == 0:
+                #position =   0  123  4567
+                #size     = 1234,5678,9012
+                #pow      = 12 4    8
+
+                # This checks 3, 5, 7, 9, 11
+                # Check bet, 1
+                if not (int(decodehammingpart[0]) + int(decodehammingpart[2]) + int(decodehammingpart[4]) + int(decodehammingpart[6]) + int(decodehammingpart[8]) + int(decodehammingpart[10])) % 2 == 0:
+                    errors.append(1)
+
+                # This checks 3, 6, 7, 10, 11
+                # Check bet, 2
+                if not (int(decodehammingpart[1]) + int(decodehammingpart[2]) + int(decodehammingpart[5]) + int(decodehammingpart[6]) + int(decodehammingpart[9]) + int(decodehammingpart[10])) % 2 == 0:
+                    errors.append(2)
+
+                # Checks 5, 6, 7, 12
+                # Check bet, 4
+                if not (int(decodehammingpart[3]) + int(decodehammingpart[4]) + int(decodehammingpart[5]) + int(decodehammingpart[6]) + int(decodehammingpart[11])) % 2 == 0:
+                    errors.append(4)
+
+                # Checks 9, 10, 11, 12
+                # Check bet, 8
+                if not (int(decodehammingpart[7]) + int(decodehammingpart[8]) + int(decodehammingpart[9]) + int(decodehammingpart[10]) + int(decodehammingpart[11])) % 2 == 0:
+                    errors.append(8)
+
+                if sum(errors) > 12:
+                    print "Multiple Bit Error, unrecoverable data"
+                if errors:
+                    if decodehammingpart[sum(errors) - 1] == "0":
+                        decodehammingpart[sum(errors) - 1] = "1"
+                    else:
+                        decodehammingpart[sum(errors) - 1] = "0"
+                    errors = []
+                    
+                t = decodehammingpart[2] + decodehammingpart[4] + decodehammingpart[5] + decodehammingpart[6]
+                decodedhamming += hexvalues[t]
+                t = ""
+
+                t = decodehammingpart[8] + decodehammingpart[9] + decodehammingpart[10] + decodehammingpart[11]
+                decodedhamming += hexvalues[t]
+                t = ""
+                
+                decodehammingpart = []
+
+                decodedhamming += ":"
+
+    print "Decoded and Corrected: %s" % decodedhamming
+
+    encodedhuffmanhex = decodedhamming
+    decodedbinary = ""
+    decodetext = []
+    decodedtext = ""
+
+    for hh in encodedhuffmanhex:
+        if hh != ":":
+            decodedbinary += hexvalues[hh]
+
+    t = ""
+    for bi in decodedbinary:
+        t += bi
+        if t in treevalues.keys():
+            if decodetext:
+                if decodetext[len(decodetext) - 1] != 'etx':
+                    decodetext.append(treevalues[t])
+                    t = ""
+            else:
+                decodetext.append(treevalues[t])
+                t = ""
+    if decodetext[0] == "stx":
+        for char in decodetext:
+            if char != "stx" and char != "etx":
+                if char != "sp":
+                    decodedtext += char
+                else:
+                    decodedtext += " "
+    else:
+        print "Decoding error, no STX"
+        
+    print "Decoded text: %s" % decodedtext
