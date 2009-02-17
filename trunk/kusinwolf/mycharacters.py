b4fc55935bfe73c93b750472bc0dc336d6ed8863
kusinwolf <kusinwolf@c446d5ca-c62c-11dd-92e3-e3085bfe42ca>
2009-02-17 01:07:45 +0000

    added functionality to EVE program

--- trunk/kusinwolf/mycharacters.py
+++ trunk/kusinwolf/mycharacters.py
@@ -2,3 +2,16 @@ from EVECharacter import *
 
 lucitania = extractXML("Lucitania.xml")
 xressmeth = extractXML("Xressmeth.xml")
+
+params = {
+    'characterID': 672389577,
+    'userid': 1690689,
+    #'apikey': 'jdFQPL18o0TvoZ63KnQeVGE1kw8KQ7iJDFYNjhxc0RMLLpfgRAz5nod5MiuJElCB'
+    'apikey': 'C35335BEE7054E13AC80AD63E7AE2FB1FD5430420671448E88AD279FCFC4BF0B'
+    }
+
+buildSkillTree(params)
+
+api = extractAPI(params)
+
+
