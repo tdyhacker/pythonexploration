24047542a6ebbcc322a32accadeb983e80288497
kusinwolf <kusinwolf@c446d5ca-c62c-11dd-92e3-e3085bfe42ca>
2009-02-18 22:16:42 +0000

    Removing old version of Pylons project

--- kusinwolf/PylonsApps/yinyang/yinyang/tests/functional/test_yiya.py
+++
@@ -1,7 +0,0 @@
-from yinyang.tests import *
-
-class TestYiyaController(TestController):
-
-    def test_index(self):
-        response = self.app.get(url_for(controller='yiya'))
-        # Test response...
