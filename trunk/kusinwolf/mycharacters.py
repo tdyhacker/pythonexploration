d2453e82cba9a6185c52524478fc1c763ba884c1
kusinwolf <kusinwolf@c446d5ca-c62c-11dd-92e3-e3085bfe42ca>
2009-02-20 23:58:13 +0000

    Updated extractXML file to look at the database and added level 0 button to the GUI

--- trunk/kusinwolf/mycharacters.py
+++ trunk/kusinwolf/mycharacters.py
@@ -1,16 +1,16 @@
-from EVECharacter import *
-from EVEDatabase import *
-from EVEGUI import *
+from EVEDatabase import extractAPI, extractXML
+from EVEGUI import Browser
+import wx
 
 # Offline method of parsing information
-#lucitania = extractXML("Lucitania.xml")
-#xressmeth = extractXML("Xressmeth.xml")
+lucitania = extractXML("Lucitania.xml")
+xressmeth = extractXML("Xressmeth.xml")
 
-lucitania = extractAPI({
-    'characterID': 672389577, # Lucitania
-    'userid': 1690689,
-    'apikey': 'jdFQPL18o0TvoZ63KnQeVGE1kw8KQ7iJDFYNjhxc0RMLLpfgRAz5nod5MiuJElCB' # Public
-    })
+#lucitania = extractAPI({
+#    'characterID': 672389577, # Lucitania
+#    'userid': 1690689,
+#    'apikey': 'jdFQPL18o0TvoZ63KnQeVGE1kw8KQ7iJDFYNjhxc0RMLLpfgRAz5nod5MiuJElCB' # Public
+#    })
 
 #xressmeth = extractAPI({
 #    'characterID': 160054538, # Xress Meth
@@ -18,12 +18,12 @@ lucitania = extractAPI({
 #    'apikey': 'd326ybm33XBUXNuek2chNTTqV9yyyu2SAAoZSax3srYk1EU4y7ziobOjjOX247LQ' # Public
 #    })
 
-## Main Application
-#app = wx.App()
-## Builds a Window
+# Main Application
+app = wx.App()
+# Builds a Window
 #Browser(None, -1, 'Character Browser', xressmeth)
-## Builds another Window
-#Browser(None, -1, 'Character Browser', lucitania)
-#app.MainLoop() # starts the entire application
+# Builds another Window
+Browser(None, -1, 'Character Browser', lucitania)
+app.MainLoop() # starts the entire application
 
 
