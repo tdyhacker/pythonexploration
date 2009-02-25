052eb40cc172be10db688cbf59e176b260ee2ea0
kusinwolf <kusinwolf@c446d5ca-c62c-11dd-92e3-e3085bfe42ca>
2009-02-25 18:50:58 +0000

    changing functionality of character object building

--- kusinwolf/EVE-Online Character Monitor/mycharacters.py
+++ kusinwolf/EVE-Online Character Monitor/mycharacters.py
@@ -2,6 +2,14 @@ from EVEDatabase import extractAPI, extractXML
 from EVEGUI import Browser
 import wx
 
+# ONLY UPDATE WITH THIS COMMAND FOR NEW DATABASES!!
+#from EVEDatabase import _buildSkillTree
+#_buildSkillTree({
+#    'characterID': 672389577, # Lucitania
+#    'userid': 1690689,
+#    'apikey': 'jdFQPL18o0TvoZ63KnQeVGE1kw8KQ7iJDFYNjhxc0RMLLpfgRAz5nod5MiuJElCB' # Public
+#    }) # Arbitray information for the pull
+
 # Offline method of parsing information
 #lucitania = extractXML("Lucitania.xml")
 #xressmeth = extractXML("Xressmeth.xml")
@@ -18,10 +26,10 @@ xressmeth = extractAPI({
     'apikey': 'd326ybm33XBUXNuek2chNTTqV9yyyu2SAAoZSax3srYk1EU4y7ziobOjjOX247LQ' # Public
     })
 
-# Main Application
-app = wx.App()
-# Builds a Window
-Browser(None, -1, 'Character Browser', xressmeth)
-# Builds another Window
-#Browser(None, -1, 'Character Browser', lucitania)
-app.MainLoop() # starts the entire application
+## Main Application
+#app = wx.App()
+## Builds a Window
+#Browser(None, -1, 'Character Browser', xressmeth)
+## Builds another Window
+##Browser(None, -1, 'Character Browser', lucitania)
+#app.MainLoop() # starts the entire application
