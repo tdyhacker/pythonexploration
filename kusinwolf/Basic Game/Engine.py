55e7f3f2deb695ecc9abedabf70e8b6b7aceee01
kusinwolf <kusinwolf@c446d5ca-c62c-11dd-92e3-e3085bfe42ca>
2009-04-07 22:37:07 +0000

    Playing with Basic Game, going to be looking at a wxPython + PyOpenGL setup to try my hand at 3D graphics

--- kusinwolf/Basic Game/Engine.py
+++ kusinwolf/Basic Game/Engine.py
@@ -1,108 +1,113 @@
 import os, sys
 import pygame
+from pygame import *
 from pygame.locals import *
+from Character import *
 
-if not pygame.font: print 'Warning, fonts disabled'
-if not pygame.mixer: print 'Warning, sound disabled'
+# Look into Panda 3D
+# Look into PyOpenGL
+# http://showmedo.com/videos/video?name=1510000&fromSeriesID=151
 
+# Just the Engine mechanics
 
-def load_image(name, colorkey=None):
-    fullname = os.path.join('PYGame Examples/examples/data', name)
-    try:
-        image = pygame.image.load(fullname)
-    except pygame.error, message:
-        print 'Cannot load image:', name
-        raise SystemExit, message
-    image = image.convert()
-    if colorkey is not None:
-        if colorkey is -1:
-            colorkey = image.get_at((0,0))
-        image.set_colorkey(colorkey, RLEACCEL)
-    return image, image.get_rect()
-
-
-def load_sound(name):
-    class NoneSound:
-        def play(self): pass
-    if not pygame.mixer:
-        return NoneSound()
-    fullname = os.path.join('PYGame Examples/examples/data', name)
-    try:
-        sound = pygame.mixer.Sound(fullname)
-    except pygame.error, message:
-        print 'Cannot load sound:', wav
-        raise SystemExit, message
-    return sound
-
-
-class Fist(pygame.sprite.Sprite):
-    """moves a clenched fist on the screen, following the mouse"""
+class Physics(object):
     def __init__(self):
-        pygame.sprite.Sprite.__init__(self) #call Sprite initializer
-        self.image, self.rect = load_image('fist.bmp', -1)
-        self.punching = 0
+        # On foot
+        self.forwardgroundspeed = 45 # pxl a second
+        self.backwardgroundspeed = 35 # pxl a second
+        self.groundturnspeed = 0.8 # radians a second
+        
+        # In helicoptor
+        self.forwardairspeed = 120 # pxl a second
+        self.backwardairspeed = 45 # pxl a second
+        self.airturnspeed = 0.4 # radians a second
+        
+        # In Small boat
+        self.forwardwaterspeed = 80 # pxl a second
+        self.backwardwaterspeed = 25 # pxl a second
+        self.waterturnspeed = 0.5 # radians a second
+        
+        self.screenx = 800
+        self.screeny = 600
 
-    def update(self):
-        "move the fist based on the mouse position"
-        pos = pygame.mouse.get_pos()
-        self.rect.midtop = pos
-        if self.punching:
-            self.rect.move_ip(5, 10)
-
-    def punch(self, target):
-        "returns true if the fist collides with the target"
-        if not self.punching:
-            self.punching = 1
-            hitbox = self.rect.inflate(-5, -5)
-            return hitbox.colliderect(target.rect)
-
-    def unpunch(self):
-        "called to pull the fist back"
-        self.punching = 0
-
-class Chimp(pygame.sprite.Sprite):
-    """moves a monkey critter across the screen. it can spin the
-       monkey when it is punched."""
+class Engine(object):
     def __init__(self):
-        pygame.sprite.Sprite.__init__(self) #call Sprite intializer
-        self.image, self.rect = load_image('chimp.bmp', -1)
-        screen = pygame.display.get_surface()
-        self.area = screen.get_rect()
-        self.rect.topleft = 10, 10
-        self.move = 9
-        self.dizzy = 0
-
-    def update(self):
-        "walk or spin, depending on the monkeys state"
-        if self.dizzy:
-            self._spin()
-        else:
-            self._walk()
-
-    def _walk(self):
-        "move the monkey across the screen, and turn at the ends"
-        newpos = self.rect.move((self.move, 0))
-        if not self.area.contains(newpos):
-            if self.rect.left < self.area.left or self.rect.right > self.area.right:
-                self.move = -self.move
-            newpos = self.rect.move((self.move, 0))
-            self.image = pygame.transform.flip(self.image, 1, 0)
-        self.rect = newpos
-
-    def _spin(self):
-        "spin the monkey image"
-        center = self.rect.center
-        self.dizzy += 12
-        if self.dizzy >= 360:
-            self.dizzy = 0
-            self.image = self.original
-        else:
-            rotate = pygame.transform.rotate
-            self.image = rotate(self.original, self.dizzy)
-        self.rect = self.image.get_rect(center=center)
-
-    def punched(self):
-        "this will cause the monkey to start spinning"
-        if not self.dizzy:
-            self.dizzy = 1
-            self.original = self.image
+        """this function is called when the program starts.
+           it initializes everything it needs, then runs in
+           a loop until the function returns."""
+        
+        # Starting Physics Engine
+        self.physics = Physics()
+        
+        #Initialize Everything
+        pygame.init()
+        self.screen = pygame.display.set_mode((self.physics.screenx, self.physics.screeny))
+        pygame.display.set_caption("The Test of all Tests!")
+        pygame.mouse.set_visible(1)
+    
+        #Create The Backgound
+        self.background = pygame.Surface(self.screen.get_size())
+        self.background = self.background.convert()
+        self.background.fill((250, 250, 250))
+        
+        #Display The Background
+        self.screen.blit(self.background, (0, 0))
+        pygame.display.flip()
+        
+        #Prepare Game Objects
+        self.clock = pygame.time.Clock()
+        self.pointer = Character()
+        pointer_draw = pygame.draw.circle(self.screen, (0,0,0), self.pointer.getPosition(0,0), 15)
+        
+        self.heldkeys = {}
+    
+    def mainloop(self):
+        #Main Loop
+        while 1:
+            # Clean up the background
+            self.background.fill((250, 250, 250))
+            
+            for event in self.heldkeys.itervalues():
+                event() # run the method
+            
+            #Handle Input Events
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    return False
+                
+                elif event.type == KEYDOWN:
+                    if event.key == K_DOWN:
+                        self.heldkeys[event.key] = self.ON_K_DOWN
+                    elif  event.key == K_UP:
+                        self.heldkeys[event.key] = self.ON_K_UP
+                    elif event.key == K_LEFT:
+                        self.heldkeys[event.key] = self.ON_K_LEFT
+                    elif event.key == K_RIGHT:
+                        self.heldkeys[event.key] = self.ON_K_RIGHT
+                
+                elif event.type == KEYUP:
+                    if self.heldkeys.has_key(event.key):
+                        self.heldkeys.pop(event.key)
+            
+            #Draw Everything
+            pygame.display.flip()
+    
+    def ON_K_DOWN(self):
+        self.pointer.position[1] += 2
+        self.screen.blit(self.background, (0, 0))
+        pointer_draw = pygame.draw.circle(self.screen, (0,0,0), self.pointer.getPosition(0,0), 15)
+    
+    def ON_K_UP(self):
+        self.pointer.position[1] -= 2
+        self.screen.blit(self.background, (0, 0))
+        pointer_draw = pygame.draw.circle(self.screen, (0,0,0), self.pointer.getPosition(0,0), 15)
+    
+    def ON_K_LEFT(self):
+        self.pointer.position[0] -= 2
+        self.screen.blit(self.background, (0, 0))
+        pointer_draw = pygame.draw.circle(self.screen, (0,0,0), self.pointer.getPosition(0,0), 15)
+    
+    def ON_K_RIGHT(self):
+        self.pointer.position[0] += 2
+        self.screen.blit(self.background, (0, 0))
+        pointer_draw = pygame.draw.circle(self.screen, (0,0,0), self.pointer.getPosition(0,0), 15)
