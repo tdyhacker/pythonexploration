0851d6aed96f8561435b81f0f769c3b02b8939bb
kusinwolf <kusinwolf@c446d5ca-c62c-11dd-92e3-e3085bfe42ca>
2009-05-28 22:16:57 +0000

    Creating Image management system

---
+++ trunk/kusinwolf/PylonsApps/iman/iman/websetup.py
@@ -0,0 +1,17 @@
+"""Setup the iman application"""
+import logging
+
+from iman.config.environment import load_environment
+
+log = logging.getLogger(__name__)
+
+def setup_app(command, conf, vars):
+    """Place any commands to setup iman here"""
+    load_environment(conf.global_conf, conf.local_conf)
+
+    # Load the models
+    from iman.model import meta
+    meta.metadata.bind = meta.engine
+
+    # Create the tables if they aren't there already
+    meta.metadata.create_all(checkfirst=True)
