e9b5ba7be423278aee9d04960dfbf05062dff5dc
kusinwolf <kusinwolf@c446d5ca-c62c-11dd-92e3-e3085bfe42ca>
2010-04-02 00:09:47 +0000

    Still playing with the chessboard :D

--- trunk/kusinwolf/Basic Game/Chessboard.py
+++ trunk/kusinwolf/Basic Game/Chessboard.py
@@ -19,32 +19,55 @@ chess_board = {"A": blank_fn(),
                "H": blank_fn()
                }
 
+
 # Color layout
-chess_board_colors = {"A": color_fn_black(),
-                      "B": color_fn_white(),
-                      "C": color_fn_black(),
-                      "D": color_fn_white(),
-                      "E": color_fn_black(),
-                      "F": color_fn_white(),
-                      "G": color_fn_black(),
-                      "H": color_fn_white()
-                      }
+self.chess_board_colors = {"A": color_fn_black(),
+                            "B": color_fn_white(),
+                            "C": color_fn_black(),
+                            "D": color_fn_white(),
+                            "E": color_fn_black(),
+                            "F": color_fn_white(),
+                            "G": color_fn_black(),
+                            "H": color_fn_white()}
 
 class Piece(object):
     def __init__(self, name = "", digest = "", side = ""):
         self.name = name
         self.digest = digest
         self.side = side
+        
+        self.x = 0
+        self.y = 0
+        
+        self.board_limits = \
+            [color_fn_black(),
+            color_fn_white(),
+            color_fn_black(),
+            color_fn_white(),
+            color_fn_black(),
+            color_fn_white(),
+            color_fn_black(),
+            color_fn_white()]
     
     def __repr__(self):
         return "(%s)%s" % (self.side[0], self.digest)
-        
     
     def movement(self):
         """
         Override this method with the proper information
         """
         pass
+    
+    def checkDirection(self, angle, max_distance):
+        """angle may equal N, NE, E, SE, S, SW, W, NW """
+        if angle not in ["N", "NE", "E", "SE", "S", "SW", "W", "NW"]:
+            raise ValueError("Bad angle sent '%s' is not an angle" % angle)
+        
+        
+        
+    
+    def getPosition(self):
+        return (self.x, self.y)
     
     def putSelfInCheck(self):
         "If I move can the King be put in check?"
@@ -57,44 +80,50 @@ class Piece(object):
 class King(Piece):
     def __init__(self, side):
         Piece.__init__(self, "King", "Ki", side)
+        has_moved = False # For castling
     
     def movement(self):
-        pass
+        max_distance = 1
 
 class Queen(Piece):
     def __init__(self, side):
         Piece.__init__(self, "Queen", "Qu", side)
     
     def movement(self):
-        pass
+        max_distance = 7
 
 class Bishop(Piece):
     def __init__(self, side):
         Piece.__init__(self, "Kishop", "Bi", side)
     
     def movement(self):
-        pass
+        max_distance = 7
 
 class Knight(Piece):
     def __init__(self, side):
         Piece.__init__(self, "Knight", "Kn", side)
     
     def movement(self):
-        pass
+        max_distance = 2 # L Hook
 
 class Rook(Piece):
     def __init__(self, side):
         Piece.__init__(self, "Rook", "Ro", side)
+        has_moved = False # For castling
     
     def movement(self):
-        pass
+        max_distance = 7
 
 class Pawn(Piece):
     def __init__(self, side):
         Piece.__init__(self, "Pawn", "Pa", side)
+        has_moved = False # For opening push
     
     def movement(self):
-        pass
+        max_distance = 2 # If it has not moved
+        if has_moved:
+            max_distance = 1 # After it has moved
+            
 
 # Standard board setup
 chess_board = {"A": [Rook(WHITE),   Pawn(WHITE), '', '', '', '', Pawn(BLACK), Rook(BLACK)],
